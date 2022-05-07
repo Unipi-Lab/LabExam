da6774721940c14e0a9a0bd75c2338db0df09d47
Giovanni Braconi <thumpback2002.it@gmail.com>
2022-05-07 15:36:38 +0200

    Added Socket Connection Template, Trying to implement the connection on farm.c

--- ProgettoFarm-Lab2A/farm.c
+++ ProgettoFarm-Lab2A/farm.c
@@ -16,7 +16,6 @@
 #define SOCKNAME "./mysock"
 #define N 100
 
-   
 #include <util.h>
 
 #define MAX_FILENAME_LENGTH 255
@@ -27,26 +26,24 @@ typedef struct threadArgs
     BQueue_t *q;
     char **regularfiles;
     int nregularfiles;
-    int delay
+    int delay;
+    struct sockaddr *psa
 } threadArgs_t;
 
 void *Master(void *arg)
 {
-    
+
     int myid = ((threadArgs_t *)arg)->thid;
 
     BQueue_t *q = ((threadArgs_t *)arg)->q;
-    char **regularfiles=((threadArgs_t*)arg)->regularfiles;
+    char **regularfiles = ((threadArgs_t *)arg)->regularfiles;
     int nregularfiles = ((threadArgs_t *)arg)->nregularfiles;
     int delay = ((threadArgs_t *)arg)->delay;
-    int microsecondsdelay=delay*1000;
-    
-
-    
+    int microsecondsdelay = delay * 1000;
 
     for (int i = 0; i < nregularfiles; i++)
     {
-        char **data = malloc(sizeof(char)*MAX_FILENAME_LENGTH);
+        char **data = malloc(sizeof(char) * MAX_FILENAME_LENGTH);
         if (data == NULL)
         {
             perror("Producer malloc");
@@ -58,41 +55,59 @@ void *Master(void *arg)
             fprintf(stderr, "Errore: push\n");
             pthread_exit(NULL);
         }
-        printf("Master %d pushed <%s>\n", myid,*data);
+        printf("Master %d pushed <%s>\n", myid, *data);
         usleep(microsecondsdelay);
     }
-    
+
     printf("The master finished\n");
-    
+
     return NULL;
 }
 void *Worker(void *arg)
 {
-    
-   // printf("ITS ME ... WORKER\n");
+
+    // printf("ITS ME ... WORKER\n");
     int myid = ((threadArgs_t *)arg)->thid;
 
     BQueue_t *q = ((threadArgs_t *)arg)->q;
+    struct sockaddr *psa = ((threadArgs_t *)arg)->psa;
+
     size_t consumed = 0;
-    long  num=0;
-    long sum=0;
-    long i=0;
+    long num = 0;
+    long sum = 0;
+    long i = 0;
+
+    int fd_skt;
+    char buf[N];
+    fd_skt = socket(AF_UNIX, SOCK_STREAM, 0);
+
+    // try to connect to collector
+    while (connect(fd_skt, (struct sockaddr *)psa,
+
+                   sizeof(*psa)) == -1)
+    {
+        if (errno == ENOENT)
+            sleep(1);
+        else
+            exit(EXIT_FAILURE);
+    }
+
     while (1)
     {
-       
+
         char **data;
         data = pop(q);
-       // printf("Worker %d popped : <%s>\n",myid,data);
+        // printf("Worker %d popped : <%s>\n",myid,data);
         assert(data);
-       // printf("AFTER pop in worker\n");
-        if (strcmp(data , "-1")==0)
+        // printf("AFTER pop in worker\n");
+        if (strcmp(data, "-1") == 0)
         {
             printf("BEFORE free in worker\n");
 
             free(data);
             break;
         }
-    
+
         ++consumed;
 
         printf("Worker %d extracted <%s>\n", myid, *data);
@@ -104,66 +119,66 @@ void *Worker(void *arg)
         {
             perror("fopen");
             return NULL;
-           
         }
-        
+
         // finche' ho linee da leggere vado avanti
         while (fread(&num, sizeof(long), 1, in) != 0)
         {
-            
-           sum+=(num*i);
-           i++;
+
+            sum += (num * i);
+            i++;
         }
-        
+
+        write(fd_skt, *data, MAX_FILENAME_LENGTH);
+        read(fd_skt, buf, N);
+        printf("Client got: % s\n", buf);
+
         // inserisco l'EOS nella coda di uscita
-    
+
         fclose(in);
 
-       
         free(data);
     }
-
+    close(fd_skt);
     printf("Worker %d summed a total of <%ld>\n", myid, sum);
 
     printf("Worker %d consumed <%ld> files\n", myid, consumed);
-    
-   printf("Worker %d finished\n",myid);
+
+    printf("Worker %d finished\n", myid);
     return NULL;
 }
 
-
-void MasterWorker(int nthread,int qlen,int delay,int nfiles,char **filenames)
+void MasterWorker(int nthread, int qlen, int delay, int nfiles, char **filenames)
 {
-   size_t filesize;
-   char **regularfiles = malloc(MAX_FILENAME_LENGTH * sizeof(char) * 1000);
-   int nregularfiles=0;
+    size_t filesize;
+    char **regularfiles = malloc(MAX_FILENAME_LENGTH * sizeof(char) * 1000);
+    int nregularfiles = 0;
 
-   int fd_skt, fd_c;
-   char buf[N];
-   struct sockaddr_un sa;
-   strncpy(sa.sun_path, SOCKNAME, UNIX_PATH_MAX);
-   sa.sun_family = AF_UNIX;
+    struct sockaddr_un sa; /* ind AF_UNIX */
+    strcpy(sa.sun_path, SOCKNAME);
+    sa.sun_family = AF_UNIX;
 
-   for (int i = 0; i < nfiles; i++)
-   {
-       if (isRegular(filenames[i], &filesize) != 1)
-       { // check if the file is regular ,and get the file size
-           if (errno == 0)
-           {
-               fprintf(stderr, "%s non e' un file regolare\n", filenames[i]);
-               continue;
-           }
-           perror("isRegular");
-           continue;
-       }
-      
-       regularfiles[nregularfiles]=filenames[i];
-       printf("Regular filename: %s\n", regularfiles[nregularfiles]);
-       nregularfiles++;
+    run_server(&sa); /*collector*/
+
+    for (int i = 0; i < nfiles; i++)
+    {
+        if (isRegular(filenames[i], &filesize) != 1)
+        { // check if the file is regular ,and get the file size
+            if (errno == 0)
+            {
+                fprintf(stderr, "%s non e' un file regolare\n", filenames[i]);
+                continue;
+            }
+            perror("isRegular");
+            continue;
+        }
+
+        regularfiles[nregularfiles] = filenames[i];
+        printf("Regular filename: %s\n", regularfiles[nregularfiles]);
+        nregularfiles++;
     }
 
-   
-printf("Allocating memory for threads and threadargs\n");
+    printf("Allocating memory for threads and threadargs\n");
     pthread_t *th;
     threadArgs_t *thArgs;
     th = malloc((nthread + 1) * sizeof(pthread_t));
@@ -181,109 +196,85 @@ printf("Allocating memory for threads and threadargs\n");
         printf("Queue creation error");
         exit(EXIT_FAILURE);
     }
-//setting args for Master and Workers
-printf("Setting args\n");
+    // setting args for Master and Workers
+    printf("Setting args\n");
     thArgs[0].thid = 0;
     thArgs[0].q = q;
-    thArgs[0].regularfiles=regularfiles;
-    thArgs[0].nregularfiles=nregularfiles;
-    thArgs[0].delay=delay;
+    thArgs[0].regularfiles = regularfiles;
+    thArgs[0].nregularfiles = nregularfiles;
+    thArgs[0].delay = delay;
 
-
-
-    for (int i = 0; i < nthread ; i++)
+    for (int i = 0; i < nthread; i++)
     {
-        thArgs[i+1].thid = i+1;
-        thArgs[i+1].q = q;
-        
+        thArgs[i + 1].thid = i + 1;
+        thArgs[i + 1].q = q;
+        thArgs[i + 1].psa = &sa;
     }
 
-    
     printf("creating threads\n");
-    
-//creating Master and Workers
-    
-        if (pthread_create(&th[0], NULL, Master, &thArgs[0]) != 0)
+
+    // creating Master and Workers
+
+    if (pthread_create(&th[0], NULL, Master, &thArgs[0]) != 0)
+    {
+        printf("Error in master creation");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < nthread; i++)
+    {
+        if (pthread_create(&th[i + 1], NULL, Worker, &thArgs[i + 1]) != 0)
         {
-            printf("Error in master creation");
+            printf("Error in worker creation");
             exit(EXIT_FAILURE);
         }
-        for (int i = 0; i < nthread; i++)
-        {
-            if (pthread_create(&th[i + 1], NULL, Worker,&thArgs[i+1]) != 0)
+    }
+    /*
+        printf("creating collector \n");
+         //creating Collector process
+            pid_t pid = fork();
+
+            if (pid == -1)
             {
-                printf("Error in worker creation");
+                perror("fork failed");
                 exit(EXIT_FAILURE);
             }
-        }
-
-    printf("creating collector \n");
-     //creating Collector process
-        pid_t pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid != 0)
-        {
-            //MasterWorker
-            fd_skt = socket(AF_UNIX, SOCK_STREAM, 0);
-            bind(fd_skt, (struct sockaddr *)&sa,
-
-                 sizeof(sa));
-
-            listen(fd_skt, SOMAXCONN);
-            fd_c = accept(fd_skt, NULL, 0);
-            read(fd_c, buf, N);
-            printf("Server got: % s\n", buf);
-            write(fd_c, "Bye !", 5);
-            close(fd_skt);
-            close(fd_c);
-            exit(EXIT_SUCCESS);
-
-            
-            int status;
-            // Wait for Collector
-            waitpid(pid, &status, 0);
-
-            
-        }
-        else
-        {
-            // Collector
-            fd_skt = socket(AF_UNIX, SOCK_STREAM, 0);
-            while (connect(fd_skt, (struct sockaddr *)&sa,
-
-                           sizeof(sa)) == -1)
+            else if (pid == 0)
             {
-                if (errno == ENOENT)
+                // Collector
 
-                    sleep(1); /* sock non esiste */
 
-                else
-                    exit(EXIT_FAILURE);
+
+
+
+
+
+                exit(EXIT_SUCCESS);
+
+
+
             }
+            else
+            {
+                //MasterWorker
 
-            write(fd_skt, "Hallo !", 7);
-            read(fd_skt, buf, N);
-            printf("Client got: % s\n", buf);
-            close(fd_skt);
 
-            exit(EXIT_SUCCESS);
-        }
+
+                int status;
+                // Wait for Collector
+                waitpid(pid, &status, 0);
+            }
+            */
 
     // waiting master and workers
     printf("About to wait for master and workers\n");
     pthread_join(th[0], NULL);
-    
-  char *end="-1";
+
+    char *end = "-1";
     for (int i = 0; i < nthread; i++)
     {
 
-        char *eos = malloc(sizeof(char)*MAX_FILENAME_LENGTH);
-        strcpy(eos ,end);
+        char *eos = malloc(sizeof(char) * MAX_FILENAME_LENGTH);
+        strcpy(eos, end);
         push(q, eos);
     }
     for (int i = 0; i < nthread; i++)
@@ -291,19 +282,17 @@ printf("Setting args\n");
         pthread_join(th[i + 1], NULL);
     }
 
-    deleteBQueue(q,NULL);
+    deleteBQueue(q, NULL);
     free(th);
     free(thArgs);
-        
-        printf("END OF MASTER WORKER\n");
-        return NULL;
+
+    printf("END OF MASTER WORKER\n");
+    return NULL;
 }
-
-
 
 void usage(char *pname)
 {
-    
+
     fprintf(stderr, "\nUsage: %s filename1 filename 2 filename3 ...optional: -n nthreads -q qlen -t delay\n", pname);
     exit(EXIT_FAILURE);
 }
@@ -312,11 +301,11 @@ int main(int argc, char *argv[])
     char **filenames = malloc(MAX_FILENAME_LENGTH * sizeof(char) * 1000);
     int nthread = 4;
     int qlen = 8;
-    int delay=0;
-    int nfiles=0;
+    int delay = 0;
+    int nfiles = 0;
     extern char *optarg;
-    int  opt;
-    int listBeginning=1;
+    int opt;
+    int listBeginning = 1;
 
     // parsing degli argomenti
     while ((opt = getopt(argc, argv, "n:q:t:")) != -1)
@@ -324,7 +313,7 @@ int main(int argc, char *argv[])
         switch (opt)
         {
         case 'n':
-            listBeginning+=2;
+            listBeginning += 2;
             nthread = atoi(optarg);
             break;
         case 'q':
@@ -336,33 +325,31 @@ int main(int argc, char *argv[])
             delay = atoi(optarg);
             break;
         default:
-            
+
             break;
         }
-
     }
-    nfiles=argc-listBeginning;
-    printf("Number of arguments %d\n",argc);
+    nfiles = argc - listBeginning;
+    printf("Number of arguments %d\n", argc);
     if (argc < 2)
     {
         usage(argv[0]);
     }
-    printf("n=%d q=%d t=%d \n",nthread,qlen,delay);
+    printf("n=%d q=%d t=%d \n", nthread, qlen, delay);
 
-    for(int i=listBeginning;i<argc;i++)
+    for (int i = listBeginning; i < argc; i++)
     {
-       
-        filenames[i-listBeginning]=argv[i];
+
+        filenames[i - listBeginning] = argv[i];
     }
-    for (int i =0; i < nfiles; i++)
+    for (int i = 0; i < nfiles; i++)
     {
         printf("Filename: %s\n", filenames[i]);
-        
     }
 
     MasterWorker(nthread, qlen, delay, nfiles, filenames);
 
-        printf("End of main\n");
+    printf("End of main\n");
 
     return 0;
 }
--- ProgettoFarm-Lab2A/sockettest.c
+++ ProgettoFarm-Lab2A/sockettest.c
@@ -13,9 +13,103 @@
 
 #include <sys/un.h>       /* ind AF_UNIX */
 #define UNIX_PATH_MAX 108 /* man 7 unix */
-#define SOCKNAME "./mysock"
+#define SOCKNAME "./farm.sck"
 #define N 100
-int main (void) {
+
+static void run_server(struct sockaddr *psa)
+{
+    int fd_sk, fd_c, fd_num = 0, fd;
+    char buf[N];
+    fd_set set, rdset;
+    int nread;
+    fd_sk = socket(AF_UNIX, SOCK_STREAM, 0);
+    bind(fd_sk, (struct sockaddr *)psa, sizeof(*psa));
+    listen(fd_sk, SOMAXCONN);
+    if (fd_sk > fd_num)
+        fd_num = fd_sk;
+    FD_ZERO(&set);
+    FD_SET(fd_sk, &set);
+    while (1)
+    {
+        rdset = set;
+        if (select(fd_num + 1, &rdset, NULL, NULL, NULL) == -1)
+        { /* gest errore */
+            perror("select");
+            exit(EXIT_FAILURE);
+        }
+        else
+        { /* select OK */
+            for (fd = 0; fd <= fd_num; fd++)
+            {
+                if (FD_ISSET(fd, &rdset))
+                {
+                    if (fd == fd_sk)
+                    { /* sock connect pronto */
+                        fd_c = accept(fd_sk, NULL, 0);
+                        FD_SET(fd_c, &set);
+                        if (fd_c > fd_num)
+                            fd_num = fd_c;
+                    }
+                    else
+                    { /* sock I/0 pronto */
+                        nread = read(fd, buf, N);
+                        if (nread == 0)
+                        { /* EOF client finito */
+                            FD_CLR(fd, &set);
+                            while (!FD_ISSET(fd_num, &set))
+                                fd_num--;
+                            close(fd);
+                        }
+                        else
+                        { /* nread !=0 */
+                            printf("Server got : % s\n", buf);
+                            write(fd, "Bye !", 5);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void run_client(struct sockaddr *psa)
+{
+    if (fork() == 0)
+    { /* figlio, client */
+
+        int fd_skt;
+        char buf[N];
+        fd_skt = socket(AF_UNIX, SOCK_STREAM, 0);
+        while (connect(fd_skt, (struct sockaddr *)psa,
+
+                       sizeof(*psa)) == -1)
+        {
+            if (errno == ENOENT)
+                sleep(1);
+            else
+                exit(EXIT_FAILURE);
+        }
+        write(fd_skt, "Hallo !", 7);
+        read(fd_skt, buf, N);
+        printf("Client got: % s\n", buf);
+        close(fd_skt);
+        exit(EXIT_SUCCESS);
+    }
+}
+
+int main(void)
+{
+
+    struct sockaddr_un sa; /* ind AF_UNIX */
+    strcpy(sa.sun_path, SOCKNAME);
+    sa.sun_family = AF_UNIX;
+    for (int i = 0; i < 4; i++)
+        run_client(&sa); /* attiv client*/
+    run_server(&sa);     /* attiv server */
+    return 0;
+}
+
+/*
 int fd_skt, fd_c;
 char buf[N];
 struct sockaddr_un sa;
@@ -23,48 +117,49 @@ strncpy(sa.sun_path, SOCKNAME,UNIX_PATH_MAX);
 sa.sun_family=AF_UNIX;
 pid_t pid = fork();
 if (pid != 0)
-{ /* padre, server */
-    if((fd_skt = socket(AF_UNIX,SOCK_STREAM,0))==-1)
-    {
-        perror("socket");
-        return(EXIT_FAILURE);
-    }
-    printf("socket number :%d \n",fd_skt);
-    if (bind(fd_skt, (struct sockaddr *)&sa, sizeof(sa)) == -1)
-    {
-        perror("bind");
-        return (EXIT_FAILURE);
-    }
-    if (listen(fd_skt, SOMAXCONN) == -1)
-    {
-        perror("listen");
-        return (EXIT_FAILURE);
-    }
-    if (fd_c = accept(fd_skt, NULL, 0) == -1)
-    {
-        perror("accept");
-        return (EXIT_FAILURE);
-    }
-    if (read(fd_c, buf, N) == -1)
-    {
-        perror("read");
-        return (EXIT_FAILURE);
-    }
-    printf("Server got: % s\n", buf);
-    if (write(fd_c, "Bye !", 5) == -1)
-    {
-        perror("write");
-        return (EXIT_FAILURE);
-    }
-    
-    
-    close(fd_skt);
-    close(fd_c);
-    exit(EXIT_SUCCESS);
+{ // padre, server
+if ((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+{
+    perror("socket");
+    return (EXIT_FAILURE);
+}
+printf("socket number :%d \n", fd_skt);
+if (bind(fd_skt, (struct sockaddr *)&sa, sizeof(sa)) == -1)
+{
+    perror("bind");
+    return (EXIT_FAILURE);
+}
+if (listen(fd_skt, SOMAXCONN) == -1)
+{
+    perror("listen");
+    return (EXIT_FAILURE);
+}
+if ((fd_c = accept(fd_skt, NULL, 0)) == -1)
+{
+    perror("accept");
+    return (EXIT_FAILURE);
+}
+
+if (read(fd_c, buf, N) == -1)
+{
+    perror("read");
+    return (EXIT_FAILURE);
+}
+
+printf("Server got: % s\n", buf);
+if (write(fd_c, "Bye !", 5) == -1)
+{
+    perror("write");
+    return (EXIT_FAILURE);
+}
+
+close(fd_skt);
+close(fd_c);
+exit(EXIT_SUCCESS);
 }
 else
-{ /* figlio, client */
-
+{ // figlio, client
+    int n;
     if ((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
     {
         perror("socket");
@@ -74,17 +169,22 @@ else
     {
         if (errno == ENOENT)
 
-            sleep(1); /* sock non esiste */
+            sleep(1); // sock non esiste
 
         else
             exit(EXIT_FAILURE);
     }
-    write(fd_skt, "Hallo !", 7) ;
-    
-   
+
+    if ((n = write(fd_skt, "Hallo !", 7)) == -1)
+    {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
     read(fd_skt, buf, N);
     printf("Client got : % s\n", buf);
     close(fd_skt);
+    remove("farm.sck");
     exit(EXIT_SUCCESS);
 }
-}
+*/
